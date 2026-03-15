1329b625d91581746afe8e9a19861a07627085df
s0up4brains <liamrjames@icloud.com>
2026-03-15 13:31:37 +0000

    Split Dataset into train/test data (12/2/26)
    Split the dataset into user size of 400, splitting each into a training and testing dataset sample
    
    *commit should be dated 12/2/26 but failed

--- initial.m
+++ initial.m
@@ -8,3 +8,26 @@ dataTable = readtable(filePath);
 %Remove first 3 rows
 dataTable(:,[1 2 3]) = [];
 
+X = dataTable;
+n = height(X);
+
+userSize = 400; %User sample size
+splitSize = userSize / 2; %Split for train/test
+
+trainIdx = false(n,1);
+testIdx  = false(n,1);
+
+for i = 1:userSize:n
+    trainRange = i:min(i+splitSize-1, n);
+    testRange  = (i+splitSize):min(i+userSize-1, n);
+
+    trainIdx(trainRange) = true;
+    testIdx(testRange) = true;
+end
+
+% % Training Dataset - First 200 password entries per user
+trainData = X(trainIdx, :);
+% % Testing Dataset - Last 200 password entries per user
+testData  = X(testIdx, :);
+
+
